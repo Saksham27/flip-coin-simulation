b7669b9bcdc37a30bdf78ee41ba310be02d587a3
Saksham27 <geekysaksham27@gmail.com>
2020-03-20 13:05:23 +0530

    Added: Implemented singlet simulator and stored win  percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,19 +1,41 @@
 #!/bin/bash -x
 
 # constants
-HEADS=1
-TAILS=0
+HEADS=1 # TAILS wil be 0
+NO_OF_SIMULATIONS=10
+
+# declaring a dictionary
+declare -A singletData
+
+# variables
+HCount=0
+TCount=0
+HPercentage=0
+TPercentage=0
 
 # function to flip coin
 function flipCoin() {
 	local coin=$((RANDOM%2))
 	if [ $coin -eq $HEADS ]
 	then
-		echo "heads"
+		echo "H"
 	else
-		echo "tails"
+		echo "T"
 	fi
 }
 
-# simulating one coin flip
-flipCoin
+# simulating singlet coin flip
+for (( i=1; i<=$NO_OF_SIMULATIONS; i++ ))
+do
+	temp=$( flipCoin )
+	(( "$temp"Count++ ))
+	if  [ singletData[$temp] ]
+	then
+		(( singletData[$temp]++ ))
+	else
+		singletData[$temp]=1
+	fi
+done
+
+HPercentage=`echo "scale=2; ($HCount/$NO_OF_SIMULATIONS)*100" | bc` # singlet Head percentage
+TPercentage=`echo "scale=2; ($TCount/$NO_OF_SIMULATIONS)*100" | bc` # singlet Tail percentage
